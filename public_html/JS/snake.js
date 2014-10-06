d22b75dc9eadfa27ddeb56a348d6799a0f14fc1e
Antonio Obedicen <obedicena@yahoo.com>
2014-10-06 12:10:26 -0700

    milestone one

--- public_html/JS/snake.js
+++ public_html/JS/snake.js
@@ -6,6 +6,13 @@ var screenHeight;
 
 function gameInitilizer () {
     var canvas = document.getElementById("game-screen");
+    canvas= context.getContext ("2d");
+    
+    screenWidth=window.innerWidth;
+    screenHeight=window.innerHeight;
+    
+    canvas.width=screenWidth;
+    canvas.height=screenHeight;
 }
 
 function gameLoop (){
