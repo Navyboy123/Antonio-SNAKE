6b8e867c28f451f64c926b738164cbbdb4525041
Antonio Obedicen <obedicena@yahoo.com>
2014-10-08 11:27:43 -0700

    snake game

--- public_html/JS/snake.js
+++ public_html/JS/snake.js
@@ -7,7 +7,9 @@ var screenWidth;
 var screenHeight;
 
 gameInitilizer();
+snakeInitilize();
 gameDraw();
+snakeDraw();
 
 function gameInitilizer () {
     var canvas = document.getElementById("game-screen");
@@ -30,11 +32,23 @@ function gameDraw () {
 }
 
 function snakeInitilize (){
+    snake=[];
+    snakeLength=5;
+    snakeSize=20;
     
+    for(var index = 0; index < snakeLength; index++){
+        snake.push({
+            x:index,
+            y:0        
+        });
+    }
 } 
 
 function snakeDraw (){
-    
+    for (var index = 0; index < snake.length; index++){
+     context.fillstyle = "white";
+     context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
 }
 
 function snakeUpdate () {
